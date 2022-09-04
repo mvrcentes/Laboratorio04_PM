4020a1fbf36ad6d5fab467283fffa0612ac67178
Marco Ramirez <mvrcentes@hotmail.com>
2022-09-03 21:28:21 -0600

    Ejercicio No. 1 terminado

--- Ejercicio01.cpp
+++ Ejercicio01.cpp
@@ -3,6 +3,10 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <iostream>
+#include <iostream>
+#include <iterator>
+#include <list>
+
 
 //Variables 
 double totalSold, varCost, monthUtility;
@@ -101,12 +105,12 @@ struct Parameters{
   int id;
   double totalSold, varCost, monthUtility;
   Producto products[TOTAL_PRODUCTS];
+//   std::list<std::list<Producto> > nested_list;
 };
 
 void *soldReport(void *arg){
     Parameters *ps; 
     ps = (Parameters *)arg;
-
     std::cout << ps->products[ps->id].getNombre() << ":\tQ" << (ps->products[ps->id].ventasPorProducto()) << std::endl;
 
     double* res = (double*)malloc(sizeof(double));
@@ -121,7 +125,7 @@ void *varUtilityReport(void *arg){
 
     std::cout << ps->products[ps->id].getNombre() << ":\tQ" <<(ps->products[ps->id].calcularUtilidad()) << std::endl;
     double* res = (double*)malloc(sizeof(double));
-    *res = ps->totalSold += ps->products[ps->id].getProductSales();
+    *res = ps->monthUtility += ps->products[ps->id].getProductSales();
     return res;
     pthread_exit(NULL);
 };
@@ -132,7 +136,7 @@ void *varCostReport(void *arg){
 
     // std::cout << ps->products[ps->id].getNombre() << ":\tQ" <<(ps->products[ps->id].calcularUtilidad()) << std::endl;
     double* res = (double*)malloc(sizeof(double));
-    *res = ps->totalSold += ps->products[ps->id].getVarCost();
+    *res = ps->varCost += ps->products[ps->id].getVarCost();
     return res;
     pthread_exit(NULL);
 };
@@ -141,59 +145,139 @@ int main(){
     pthread_t tid;
     Parameters p;
     fflush(stdin);
+    int mes;
 
+    
 
     //Producto  nombre  | precio por unidad | costo por unidad | unidades vendidas | ventas totales | utilidad | ventas del mes | costo de varianza | utilidad del mes
-    p.products[0] = Producto(
-                            "Porcion pastel de chocolate", //nombre
-                            60, //precio por unidad
-                            20, //costo por unidad
-                            300, //unidades vendidas
-                            18000, //ventas totales
-                            12000, //utilidad
-                            0, //ventas del mes
-                            0, //costo de varianza
-                            0); //utilidad del mes
 
-    p.products[1] = Producto("White mocha", 32, 19, 400, 12800, 5120, 0, 0, 0);
-    p.products[2] = Producto("Cafe americano 8onz", 22, 13.20, 1590, 34980, 13992, 0, 0, 0);
-    p.products[3] = Producto("Latte 8onz", 24, 17.20, 200, 4800, 1360, 0, 0, 0);
-    p.products[4] = Producto("Toffee Coffe", 500, 50, 50, 5000, 500, 0, 0, 0);
-    p.products[5] = Producto("Cappuccino 8onz", 600, 60, 60, 6000, 600, 0, 0, 0);
-    p.products[6] = Producto("Smore late", 700, 70, 70, 7000, 700, 0, 0, 0);
-    p.products[7] = Producto("Cafe tostado molido", 800, 80, 80, 8000, 800, 0, 0, 0);
+    double tSold;
+    double tVarCost;
+    double tUtilityMonth; 
+    bool salir = false;
+    
+    while (!salir){
+        std::cout << "Ingrese el mes: \n[1]\tJulio\n[2]\tAgosto\n";
+        std::cin >> mes;
 
-    printf ("\n ---------------------------------------------\n");
-    printf ("Ventas por producto \n\n");
-    for (double i=0; i < TOTAL_PRODUCTS; i++){
-        p.id = i;
-        // std::cout << i << ": " << p.products[i].getNombre() << std::endl;
-        pthread_create(&tid, NULL, soldReport, ( void *)&p);
-        pthread_join(tid, &thread_result_Sold);
+        switch(mes){
+            case 1:
+                p.products[0] = Producto(
+                        "Porcion pastel de chocolate", //nombre
+                        60, //precio por unidad
+                        20, //costo por unidad
+                        300, //unidades vendidas
+                        18000, //ventas totales
+                        12000, //utilidad
+                        0, //ventas del mes
+                        0, //costo de varianza
+                        0); //utilidad del mes
+
+                p.products[1] = Producto("White mocha", 32, 19.20, 400, 12800, 5120, 0, 0, 0);
+                p.products[2] = Producto("Cafe americano 8onz", 22, 13.20, 1590, 34980, 13992, 0, 0, 0);
+                p.products[3] = Producto("Latte 8onz", 24, 17.20, 200, 4800, 1360, 0, 0, 0);
+                p.products[4] = Producto("Toffee Coffe", 28, 20.10, 390, 10920, 3081, 0, 0, 0);
+                p.products[5] = Producto("Cappuccino 8onz", 24, 17.20, 1455, 34920, 25600, 0, 0, 0);
+                p.products[6] = Producto("Smore late", 32, 23, 800, 25600, 7200, 0, 0, 0);
+                p.products[7] = Producto("Cafe tostado molido", 60, 20, 60, 3600, 2400, 0, 0, 0);
+
+                printf ("\n ---------------------------------------------\n");
+                printf ("\n Reporte del mes de Julio\n");
+
+                printf ("Ventas por producto \n\n");
+                for (double i=0; i < TOTAL_PRODUCTS; i++){
+                    p.id = i;
+                    // std::cout << i << ": " << p.products[i].getNombre() << std::endl;
+                    pthread_create(&tid, NULL, soldReport, ( void *)&p);
+                    pthread_join(tid, &thread_result_Sold);
+                }
+
+                printf ("\n --------------------------------------------- \n");
+                printf ("Utilidad por producto \n\n");
+                for (double k=0; k < TOTAL_PRODUCTS; k++){
+                    p.id = k;
+                    pthread_create(&tid, NULL, varUtilityReport, ( void *)&p);
+                    pthread_join(tid, &thread_result_MonthUtility);
+
+                    pthread_create(&tid, NULL, varCostReport, ( void *)&p);
+                    pthread_join(tid, &thread_result_VarCost);
+
+                }
+
+                printf ("\n --------------------------------------------- \n");
+
+                tSold = *(double*) thread_result_Sold;
+                tVarCost = *(double*) thread_result_VarCost;
+                tUtilityMonth= *(double*) thread_result_MonthUtility;
+
+                free(thread_result_Sold);
+                std::cout << "Total de venta: \t" << tSold << std::endl;
+                std::cout << "Total de costos variables: \t" << tVarCost << std::endl;
+                std::cout << "Total de utilidad: \t" << tUtilityMonth << std::endl;
+                printf ("\n --------------------------------------------- \n\n\n\n");
+
+                p.totalSold = 0;
+                p.varCost = 0;
+                p.monthUtility = 0;
+                break;
+            case 2:
+                p.products[1] = Producto("Porcion de pastel", 60, 20, 250, 15000, 10000, 0, 0, 0);
+                p.products[1] = Producto("White mocha", 32, 19.20, 380, 12160, 4864, 0, 0, 0);
+                p.products[2] = Producto("Cafe americano 8onz", 22, 13.20, 800, 17600, 7040, 0, 0, 0);
+                p.products[3] = Producto("Latte 8onz", 24, 17.20, 250, 6000, 1700, 0, 0, 0);
+                p.products[4] = Producto("Toffee Coffe", 28, 20.10, 600, 16800, 4740, 0, 0, 0);
+                p.products[5] = Producto("Cappuccino 8onz", 24, 17.20, 1200, 28800, 8160, 0, 0, 0);
+                p.products[6] = Producto("Smore late", 32, 23, 1540, 49280, 13860, 0, 0, 0);
+                p.products[7] = Producto("Cafe tostado molido", 60, 20, 15, 900, 600, 0, 0, 0);
+
+
+                printf ("\n ---------------------------------------------\n");
+                printf ("\n Reporte del mes de Agosto\n");
+
+                printf ("Ventas por producto \n\n");
+                for (double i=0; i < TOTAL_PRODUCTS; i++){
+                    p.id = i;
+                    // std::cout << i << ": " << p.products[i].getNombre() << std::endl;
+                    pthread_create(&tid, NULL, soldReport, ( void *)&p);
+                    pthread_join(tid, &thread_result_Sold);
+                }
+
+                printf ("\n --------------------------------------------- \n");
+                printf ("Utilidad por producto \n\n");
+                for (double k=0; k < TOTAL_PRODUCTS; k++){
+                    p.id = k;
+                    pthread_create(&tid, NULL, varUtilityReport, ( void *)&p);
+                    pthread_join(tid, &thread_result_MonthUtility);
+
+                    pthread_create(&tid, NULL, varCostReport, ( void *)&p);
+                    pthread_join(tid, &thread_result_VarCost);
+
+                }
+
+                printf ("\n --------------------------------------------- \n");
+                
+                tSold = *(double*) thread_result_Sold;
+                tVarCost = *(double*) thread_result_VarCost;
+                tUtilityMonth= *(double*) thread_result_MonthUtility;
+
+                free(thread_result_Sold);
+                std::cout << "Total de venta: \t" << tSold << std::endl;
+                std::cout << "Total de costos variables: \t" << tVarCost << std::endl;
+                std::cout << "Total de utilidad: \t" << tUtilityMonth << std::endl;
+                printf ("\n --------------------------------------------- \n\n\n\n");
+
+                p.totalSold = 0;
+                p.varCost = 0;
+                p.monthUtility = 0;      
+                break;
+                case 3:
+                    salir = true;
+            default:
+                std::cout << "Mes no valido";
+                break;
     }
 
-    printf ("\n --------------------------------------------- \n");
-    printf ("Utilidad por producto \n\n");
-    for (double k=0; k < TOTAL_PRODUCTS; k++){
-        p.id = k;
-        pthread_create(&tid, NULL, varUtilityReport, ( void *)&p);
-        pthread_join(tid, &thread_result_MonthUtility);
-
-        pthread_create(&tid, NULL, varCostReport, ( void *)&p);
-        pthread_join(tid, &thread_result_VarCost);
-
     }
-
-    printf ("\n --------------------------------------------- \n");
-    double tSold = *(double*) thread_result_Sold;
-    double tVarCost = *(double*) thread_result_VarCost;
-    double tUtilityMonth = *(double*) thread_result_MonthUtility;
-
-    free(thread_result_Sold);
-    std::cout << "Total de venta: \t" << tSold << std::endl;
-    std::cout << "Total de costos variables: \t" << tVarCost << std::endl;
-    std::cout << "Total de utilidad: \t" << tUtilityMonth << std::endl;
-    printf ("\n --------------------------------------------- \n\n\n\n");
 
     pthread_exit(NULL);
 }
